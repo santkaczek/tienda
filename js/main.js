e84a1b8c493d88a3bbc3bf991e006d2f0f7f09eb
santkaczek <santkaczek@gmail.com>
2024-07-29 03:34:50 -0300

    se carga todo menos el carrito

---
+++ js/main.js
@@ -0,0 +1,114 @@
+// productos //
+const productos = [
+    // Gorras 
+    {
+        id: "gorra-blanca",
+        titulo: "Gorra Blanca",
+        imagen: "./img/gorra blanca.jpg",
+        categoria: {
+            nombre: "Gorra",
+            id: "gorra"
+        },
+        precio: 20000
+    },
+    {
+        id: "gorra-negra",
+        titulo: "Gorra Negra",
+        imagen: "./img/gorra negra.jpg",
+        categoria: {
+            nombre: "Gorra",
+            id: "gorra"
+        },
+        precio: 20000
+    },
+    {
+        id: "gorra-blanca-y-negra",
+        titulo: "Gorra Blanca y Negra",
+        imagen: "./img/gorra blanca y negra.jpg",
+        categoria: {
+            nombre: "Gorra",
+            id: "gorra"
+        },
+        precio: 20000
+    }
+];
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("numerito")
+
+function cargarProductos(productosElegidos) {
+
+    contenedorProductos.innerHTML = "";
+    productosElegidos.forEach(producto => {
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">$${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
+        `;
+        contenedorProductos.append(div);
+    });
+    actualizarBotonesAgregar();
+}
+
+cargarProductos(productos);
+
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) =>{
+        botonesCategorias.forEach(boton.classList.remove.active)
+        e.currentTarget.classList.add("active");
+
+        if (e.currentTarget.id != "todos") {
+            const productosBoton = productos.filter(producto => producto.id === e.currentTarget.id )
+        cargarProductos(productosBoton);
+
+        } else {
+            tituloPrincipal.innerText = "Shop"
+            cargarProductos(productos);
+        }
+        
+
+
+    })
+});
+
+function actualizarBotonesAgregar() {
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+
+    botonesAgregar.forEach(boton =>{
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+
+}
+
+const productosEnCarrito = [];
+
+function agregarAlCarrito(e) {
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito(index).cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito))
+
+};
+
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito
+
+};
+
